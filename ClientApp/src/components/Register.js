8004256f2d56787e4212b9cd6018093718833686
Maria Del Carmen Jimenez Campos <majime@microsoft.com>
2021-04-27 10:51:04 +0100

    finish register form with validation, but not submit yet

--- ClientApp/src/components/Register.js
+++ ClientApp/src/components/Register.js
@@ -31,32 +31,33 @@ function Register() {
   const handleSubmit = (event) => {
     const form = event.currentTarget;
     let currentFormState = { ...formState };
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
+    let isFormValid = true;
+    
     currentFormState.emailAddress.isInvalid = false;
     if (currentFormState.emailAddress.value.length === 0 || !isValidEmailAddress(currentFormState.emailAddress.value)){
       currentFormState.emailAddress.isInvalid = true;
+      isFormValid = false;
     }
 
     currentFormState.name.isInvalid = false;
     if (currentFormState.name.value.length === 0){
       currentFormState.name.isInvalid = true;
+      isFormValid = false;
     }
 
     currentFormState.password.isInvalid = false;
-    if (currentFormState.password.value.length < 6 || !isValidPassword(currentFormState.password.value)){
+    if (!isValidPassword(currentFormState.password.value)){
       currentFormState.password.isInvalid = true;
+      isFormValid = false;
     }
 
     currentFormState.confirmPassword.isInvalid = true;
     if (currentFormState.password.value === currentFormState.confirmPassword.value){
       currentFormState.confirmPassword.isInvalid = true;
+      isFormValid = false;
     }
 
-    if (currentFormState.emailAddress.isInvalid || currentFormState.password.isInvalid || currentFormState.confirmPassword.isInvalid){
+    if (!isFormValid && form.checkValidity() === false){
       event.preventDefault();
       event.stopPropagation();
     }
@@ -68,12 +69,13 @@ function Register() {
     if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(emailAddress)){
       return true;
     } 
-    else{
-      return false;
-    }
+    return false;
   }
 
   const isValidPassword = (password) => {
+    if (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=[^a-zA-Z0-9]*).{6,20}$/.test(password)){
+      return true;
+    }
     return false;
   }
 
@@ -86,42 +88,74 @@ function Register() {
     setFormState(currentState);
   }
 
+  const handleEmailValidation = (event) => {
+    let currentState = { ...formState }
+    currentState.emailAddress.isInvalid = false;
+    if (currentState.emailAddress.value.length === 0 || !isValidEmailAddress(currentState.emailAddress.value)){
+      currentState.emailAddress.isInvalid = true;
+    }
+    setFormState(currentState);
+  }
+
+  const handleNameValidation = (event) => {
+    let currentState = { ...formState }
+    currentState.name.isInvalid = false;
+    if (currentState.name.value.length === 0){
+      currentState.name.isInvalid = true;
+    }
+    setFormState(currentState);
+  }
+
+  const handlePasswordValidation = (event) => {
+    let currentState = { ...formState }
+    currentState.password.isInvalid = false;
+    if (!isValidPassword(currentState.password.value)){
+      currentState.password.isInvalid = true;
+    }
+
+    currentState.confirmPassword.isInvalid = true;
+    if (currentState.password.value === currentState.confirmPassword.value){
+      currentState.confirmPassword.isInvalid = true;
+    }
+    setFormState(currentState);
+  }
+
   return (
     <Container fluent>
       <Row className="justify-content-md-center">
         <Col xs md="6">
           <Card>
             <Card.Header>
-              Login
+              Register
             </Card.Header>
             <Card.Body>
               <Form noValidate onSubmit={handleSubmit}>
                 <Form.Group controlId="emailAddress">
                   <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" required isInvalid={formState.emailAddress.isInvalid} onChange={handleChange} />
+                  <Form.Control type="email" placeholder="Enter email" required isInvalid={formState.emailAddress.isInvalid} onChange={handleChange} onBlur={handleEmailValidation} />
                   <Form.Control.Feedback type="invalid">
-                    Please add a valid email address
+                    Email address is not correct. 
                   </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group controlId="name">
                   <Form.Label>Name</Form.Label>
-                  <Form.Control type="text" placeholder="Enter your name" required isInvalid={formState.name.isInvalid} onChange={handleChange} />
+                  <Form.Control type="text" placeholder="Enter your name" required isInvalid={formState.name.isInvalid} onChange={handleChange} onBlur={handleNameValidation} />
                   <Form.Control.Feedback type="invalid">
-                    Please choose an user name
+                    Name can't be empty
                   </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group controlId="password">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" required isInvalid={formState.password.isInvalid} onChange={handleChange} />
+                  <Form.Control type="password" placeholder="Password" required isInvalid={formState.password.isInvalid} onChange={handleChange} onBlur={handlePasswordValidation} />
                   <Form.Control.Feedback type="invalid">
-                    Please add a valid email address
+                    Password is not valid. It should have 6 to 20 characters, and at least one number, one lower case and one upper case character.
                   </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group controlId="confirmPassword">
                   <Form.Label>Confirm password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" required isInvalid={formState.confirmPassword.isInvalid} onChange={handleChange} />
+                  <Form.Control type="password" placeholder="Password" required isInvalid={formState.confirmPassword.isInvalid} onChange={handleChange} onBlur={handlePasswordValidation} />
                   <Form.Control.Feedback type="invalid">
-                    Please make sure passwords match
+                    Password doesn't match
                   </Form.Control.Feedback>
                 </Form.Group>
                 <Button variant="primary" type="submit">
